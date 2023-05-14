48e0b09fc1b588e57d4ec1b2420509fbadd5b61d
Dima <ddd12@mail.ru>
2023-05-14 23:56:58 +0300

    Добавил конфиг сервер и все настройки связанные с ним

--- planner-server/src/main/java/ru/orlov/app/plannerserver/PlannerServerApplication.java
+++ planner-server/src/main/java/ru/orlov/app/plannerserver/PlannerServerApplication.java
@@ -2,10 +2,10 @@ package ru.orlov.app.plannerserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class PlannerServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlannerServerApplication.class, args);
